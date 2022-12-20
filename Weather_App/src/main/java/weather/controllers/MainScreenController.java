fa6625e5d053911171978d1d19d4761bd55d1426
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-12-20 21:08:01 +0100

    added initialize method to MainScreenController

--- Weather_App/src/main/java/weather/controllers/MainScreenController.java
+++ Weather_App/src/main/java/weather/controllers/MainScreenController.java
@@ -1,9 +1,25 @@
 package weather.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
 
 public class MainScreenController {
     @FXML
     private StackPane mainStackPane;
+
+    @FXML
+    public void initialize(){
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/weather/MainPane.fxml"));
+        Pane pane = null;
+        try {
+            pane = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mainStackPane.getChildren().add(pane);
+    }
 }
