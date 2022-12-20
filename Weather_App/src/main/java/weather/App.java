b18c403cbb573a26f7b02964933612be2ffc6620
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-12-20 20:46:00 +0100

    created MainScreen

--- Weather_App/src/main/java/weather/App.java
+++ Weather_App/src/main/java/weather/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,6 +17,12 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        System.out.println("New Project");
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/weather/MainScreen.fxml"));
+        StackPane stackPane = loader.load();
+        Scene scene = new Scene(stackPane, 600, 400);
+        stage.setScene(scene);
+        stage.setTitle("Weather application");
+        stage.show();
+
     }
 }
