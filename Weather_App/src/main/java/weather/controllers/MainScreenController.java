ec244325b28af41dbff4c4cf9ba362416eea7a39
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-12-20 20:58:27 +0100

    renamed MainScreen.java and injectd mainStackPane to controller

---
+++ Weather_App/src/main/java/weather/controllers/MainScreenController.java
@@ -0,0 +1,9 @@
+package weather.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.StackPane;
+
+public class MainScreenController {
+    @FXML
+    private StackPane mainStackPane;
+}
