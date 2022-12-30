945cebe26e34c26dc48ea5ec1448fd6d0076245d
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-12-30 11:08:55 +0100

    added Config class with API key

--- Weather_App/src/main/java/weather/model/WeatherService.java
+++ Weather_App/src/main/java/weather/model/WeatherService.java
@@ -1,12 +1,13 @@
 package weather.model;
 
 import net.aksingh.owmjapis.core.OWM;
+import weather.config.Config;
 
 public class WeatherService {
 
     private final OWM weatherMap;
 
     public WeatherService(){
-        weatherMap = new OWM("key");
+        weatherMap = new OWM(Config.getAPIKey());
     }
 }
