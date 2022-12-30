25a1eb86852637a34cb3d6b5fd70607a6866f7a7
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-12-30 16:14:05 +0100

    added getDate to WeatherAppController

--- Weather_App/src/main/java/weather/controllers/WeatherAppController.java
+++ Weather_App/src/main/java/weather/controllers/WeatherAppController.java
@@ -1,4 +1,18 @@
 package weather.controllers;
 
-public class WeatherAppController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import weather.model.CurrentDate;
+
+public class WeatherAppController{
+
+    CurrentDate currentDate;
+    @FXML
+    private Label date;
+
+    public void setUpCurrentDate(){
+        date.setText(currentDate.getDate());
+    }
+
 }
