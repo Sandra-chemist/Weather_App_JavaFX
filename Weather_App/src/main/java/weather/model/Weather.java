2f11cd8e3d87435f3dbe7ac88fb70e9ef0834b29
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-01-02 16:59:02 +0100

    added constructor and getters to Weather model

--- Weather_App/src/main/java/weather/model/Weather.java
+++ Weather_App/src/main/java/weather/model/Weather.java
@@ -1,4 +1,29 @@
 package weather.model;
 
+import java.time.LocalDate;
+
 public class Weather {
+
+    private final String cityName;
+    private final double tempInCelsius;
+
+    private final LocalDate date;
+
+    public Weather(String cityName, double tempInCelsius, LocalDate date) {
+        this.cityName = cityName;
+        this.tempInCelsius = tempInCelsius;
+        this.date = date;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public double getTempInClesius() {
+        return tempInCelsius;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
 }
