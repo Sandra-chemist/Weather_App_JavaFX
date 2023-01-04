963c3309c1588e264ea25ce12a64b5325f2434c2
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-01-04 12:31:05 +0100

    added constants to the URL in OpenWeatherMapClient

--- Weather_App/src/main/java/weather/model/client/OpenWeatherMapClient.java
+++ Weather_App/src/main/java/weather/model/client/OpenWeatherMapClient.java
@@ -1,18 +1,21 @@
 package weather.model.client;
 
 import org.springframework.web.client.RestTemplate;
+import weather.config.Config;
 import weather.model.Weather;
 
 import java.time.LocalDate;
 
 public class OpenWeatherMapClient implements WeatherClient {
 
+    private static final String WEATHER_URL = "https://api.openweathermap.org/data/2.5/";
+    private static final String UNITS = "metric";
     private RestTemplate restTemplate = new RestTemplate();
 
     @Override
     public Weather getWeather(String cityName) {
         String response = null;
-        response = restTemplate.getForObject("https://api.openweathermap.org/data/2.5/weather?q=Warszawa&appid=3ae4059ead35a9827fe77d59cd36f4fc&units=metric", String.class);
+        response = restTemplate.getForObject(WEATHER_URL + "weather?q={city}&appid=" + Config.getAPIKey() + "&units=" + UNITS, String.class, "Warszawa");
         System.out.println(response);
         return new Weather(cityName, 10, 50, "clouds", LocalDate.now() );
     }
