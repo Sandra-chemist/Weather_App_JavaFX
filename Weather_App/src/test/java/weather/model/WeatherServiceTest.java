cdd2170a950fb016d11a77746dce70daa1e3f4d5
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-02-10 09:34:01 +0100

    added first test to app

--- Weather_App/src/test/java/weather/model/WeatherServiceTest.java
+++ Weather_App/src/test/java/weather/model/WeatherServiceTest.java
@@ -1,4 +1,33 @@
 package weather.model;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import weather.model.client.WeatherClient;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
 public class WeatherServiceTest {
+    @InjectMocks
+    private WeatherService weatherService;
+    @Mock
+    private WeatherClient weatherClient;
+    @Test
+    void shouldReturnCurrentWeatherForecast(){
+        //given
+        Weather expectedCurrentWeather = new Weather(7, 75, "light rain", "2023-02-09", null);
+        String cityName = "Ateny";
+        given(weatherClient.getWeather(cityName)).willReturn(expectedCurrentWeather);
+
+        //when
+        Weather result = weatherService.getWeather(cityName);
+
+        //then
+        assertThat(result,is(expectedCurrentWeather));
+    }
 }
